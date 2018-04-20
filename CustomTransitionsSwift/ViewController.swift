c16eb52f5b17440e292d7e2aa8fb4c28e72ae8d6
Азат Алекбаев <azat@MacBook-Pro-2.local>
2018-04-20 17:02:20 +0300

    Replaced with new project

---
+++ CustomTransitionsSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTransitionsSwift
+//
+//  Created by Азат Алекбаев on 20.04.2018.
+//  Copyright © 2018 Азат Алекбаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
